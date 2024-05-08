46d2d462e18c9a8c38d6717aae540635235db821
jlauros <janne.lauros@csc.fi>
2024-05-08 12:56:16 +0300

    Remove tests for now

---
+++ candour-id-impl/src/test/java/fi/csc/shibboleth/plugin/candourid/messaging/impl/CandourInvitationRequestPayloadTest.java
@@ -0,0 +1,16 @@
+
+package fi.csc.shibboleth.plugin.candourid.messaging.impl;
+
+import org.apache.hc.core5.http.ProtocolException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class CandourInvitationRequestPayloadTest {
+
+    private CandourInvitationRequestPayload payload = new CandourInvitationRequestPayload();
+
+    // @Test
+    public void testPayload() throws ProtocolException, JsonProcessingException {
+    }
+
+}
