bc11fa52e5ef7ac57b2c7be58ef3d19d9485180d
jlauros <janne.lauros@csc.fi>
2024-08-07 16:56:20 +0300

    Initial draft. Merge file with CandourInvitationRequest

---
+++ candourid-impl/src/main/java/fi/csc/shibboleth/plugin/candourid/messaging/impl/CandourResultRequest.java
@@ -0,0 +1,132 @@
+package fi.csc.shibboleth.plugin.candourid.messaging.impl;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.hc.client5.http.utils.Hex;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+/**
+ * Class to create Candour invitation request.
+ *
+ */
+public class CandourResultRequest {
+
+    /** Client public key. */
+    private final String publicKey;
+    /** Client hmac key. */
+    private final String hmacKey;
+    /** API uri. */
+    private final URI uri;
+    /** Request payload. */
+    private String payload;
+
+    /**
+     * Constructor.
+     * 
+     * @param apiUri          API uri
+     * @param clientPublicKey Client public key
+     * @param clientHmacKey   Client hmac key
+     */
+    public CandourResultRequest(final URI apiUri, final String clientPublicKey, final String clientHmacKey) {
+        publicKey = clientPublicKey;
+        hmacKey = clientHmacKey;
+        uri = apiUri;
+    }
+
+    /**
+     * Get client public key.
+     * 
+     * @return Client public key
+     */
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    /**
+     * Get client hmac key.
+     * 
+     * @return Client hmac key
+     */
+    public String getHmacKey() {
+        return hmacKey;
+    }
+
+    /**
+     * Get API uri.
+     * 
+     * @return API uri
+     */
+    public URI getUri() {
+        return uri;
+    }
+
+    /**
+     * Get request payload.
+     * 
+     * @return request payload
+     */
+    public String getPayload() {
+        return payload;
+    }
+
+    /**
+     * Set request payload.
+     * 
+     * @param load request payload
+     */
+    public void setPayload(String load) {
+        payload = load;
+    }
+
+    /**
+     * CandourInvitationRequest to http request.
+     * 
+     * @return http request
+     * @throws JsonProcessingException      something went wrong
+     * @throws InvalidKeyException          something went wrong
+     * @throws NoSuchAlgorithmException     something went wrong
+     * @throws IllegalStateException        something went wrong
+     * @throws UnsupportedEncodingException something went wrong
+     * @throws URISyntaxException 
+     */
+    public ClassicHttpRequest toHttpRequest() throws JsonProcessingException, InvalidKeyException,
+            NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, URISyntaxException {
+        URI requestUri = new URI(uri.toString() + "/" + payload);
+        final ClassicRequestBuilder rb = ClassicRequestBuilder.get().setUri(requestUri)
+                .setHeader("Content-Type", "application/json").setHeader("X-AUTH-CLIENT", publicKey)
+                .setHeader("X-HMAC-SIGNATURE", calculateHmac(payload)).setCharset(Charset.forName("UTF-8"));
+        return rb.build();
+
+    }
+
+    /**
+     * Calculate hmac for the payload.
+     * 
+     * @param payload payload of the request
+     * @return
+     * @throws NoSuchAlgorithmException     something went wrong
+     * @throws InvalidKeyException          something went wrong
+     * @throws JsonProcessingException      something went wrong
+     * @throws IllegalStateException        something went wrong
+     * @throws UnsupportedEncodingException something went wrong
+     */
+    private String calculateHmac(String payload) throws NoSuchAlgorithmException, InvalidKeyException,
+            JsonProcessingException, IllegalStateException, UnsupportedEncodingException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(hmacKey.getBytes(), "HmacSHA256");
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(secretKeySpec);
+        return Hex.encodeHexString(mac.doFinal(payload.getBytes("UTF-8")));
+    }
+
+}
