8e718d4a0f95c9e8f4bb2c1c3069a717367318eb
jlauros <janne.lauros@csc.fi>
2024-08-09 14:50:57 +0300

    Javadoc update

--- candourid-impl/src/main/java/fi/csc/shibboleth/plugin/candourid/impl/AbstractCandourHttpAuthenticationAction.java
+++ candourid-impl/src/main/java/fi/csc/shibboleth/plugin/candourid/impl/AbstractCandourHttpAuthenticationAction.java
@@ -1,17 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package fi.csc.shibboleth.plugin.candourid.impl;
 
 import java.io.IOException;
@@ -39,7 +25,7 @@ import net.shibboleth.shared.logic.Constraint;
 import net.shibboleth.shared.primitive.LoggerFactory;
 
 /**
- * An abstract class for OIDC actions that make synchronous HTTP requests and
+ * An abstract class for Candour actions that make synchronous HTTP requests and
  * return types of {@link Response responses}.
  * 
  * @param <T> the response type of the object returned as a result of the
