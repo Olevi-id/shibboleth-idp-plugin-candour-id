c551281ec40b4fae8a3057d3c35046461395f94a
jlauros <janne.lauros@csc.fi>
2024-08-09 09:44:37 +0300

    Content returned back

---
+++ candourid-api/src/main/java/fi/csc/shibboleth/plugin/candourid/context/navigate/FlowExecutionKeyLookupFunction.java
@@ -0,0 +1,50 @@
+package fi.csc.shibboleth.plugin.candourid.context.navigate;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.opensaml.messaging.context.navigate.ContextDataLookupFunction;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.springframework.webflow.execution.FlowExecutionContext;
+import org.springframework.webflow.execution.RequestContext;
+
+import net.shibboleth.idp.profile.context.SpringRequestContext;
+import net.shibboleth.shared.primitive.LoggerFactory;
+
+/**
+ * {@link ContextDataLookupFunction} that returns the current flow execution
+ * key.
+ */
+public class FlowExecutionKeyLookupFunction implements ContextDataLookupFunction<ProfileRequestContext, String> {
+
+    /** Class logger. */
+    @Nonnull
+    private final Logger log = LoggerFactory.getLogger(FlowExecutionKeyLookupFunction.class);
+
+    /** {@inheritDoc} */
+    @Nullable
+    public String apply(@Nullable final ProfileRequestContext input) {
+        if (input == null) {
+            return null;
+        }
+
+        final SpringRequestContext springRequestContext = input.getSubcontext(SpringRequestContext.class);
+        if (springRequestContext == null) {
+            return null;
+        }
+
+        final RequestContext requestContext = springRequestContext.getRequestContext();
+        if (requestContext == null) {
+            return null;
+        }
+
+        final FlowExecutionContext flowExecutionContext = requestContext.getFlowExecutionContext();
+        if (flowExecutionContext == null) {
+            return null;
+        }
+        return flowExecutionContext.getKey() != null ? flowExecutionContext.getKey().toString() : null;
+
+    }
+
+}
