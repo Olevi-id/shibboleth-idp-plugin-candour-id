0fdc6587c9dcc4a9f3a98eed12b28a2d9a72e6b3
jlauros <janne.lauros@csc.fi>
2024-08-14 14:10:08 +0300

    Refactoring and clean up

--- candourid-impl/src/main/java/fi/csc/shibboleth/plugin/candourid/impl/GetUserClaims.java
+++ candourid-impl/src/main/java/fi/csc/shibboleth/plugin/candourid/impl/GetUserClaims.java
@@ -1,7 +1,6 @@
 package fi.csc.shibboleth.plugin.candourid.impl;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -10,7 +9,6 @@ import java.util.Map;
 import javax.annotation.Nonnull;
 
 import org.opensaml.profile.action.ActionSupport;
-import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 
@@ -18,17 +16,25 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import fi.csc.shibboleth.plugin.candourid.messaging.impl.CandourInvitationSuccessResponsePayload;
+import fi.csc.shibboleth.plugin.candourid.CandourEventIds;
+import fi.csc.shibboleth.plugin.candourid.context.CandourContext;
 import fi.csc.shibboleth.plugin.candourid.messaging.impl.CandourResponse;
 import fi.csc.shibboleth.plugin.candourid.messaging.impl.CandourResultRequest;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
 import net.shibboleth.shared.primitive.LoggerFactory;
 
 /**
- * TODO: Test class is initialized properly before moving to doExecute. General
- * cleanup and unit tests needed.
- * 
- * TODO: Set response to TBD context for further processing.
+ * An {@link AbstractCandourHttpAuthenticationAction action} that forms and
+ * sends a result request to Candour and stores the response to
+ * {@link CandourContext}.
+ *
+ * @event {@link org.opensaml.profile.action.EventIds#PROCEED_EVENT_ID}
+ * @event {@link org.opensaml.profile.action.EventIdsEventIds.INVALID_PROFILE_CTX}
+ * @event {@link fi.csc.shibboleth.plugin.candourid.CandourEventIds.CANDOUR_API_COMM_FAILURE}
+ * @event {@link fi.csc.shibboleth.plugin.candourid.CandourEventIds.CANDOUR_API_RESP_FAILURE}
+ * @event {@link fi.csc.shibboleth.plugin.candourid.CandourEventIds.CANDOUR_API_RESP_MALFORMED}
+ * @post {@link CandourContext#getResultClaims()} returns response claims.
+ *       response.
  */
 public class GetUserClaims extends AbstractCandourHttpAuthenticationAction {
 
@@ -36,48 +42,12 @@ public class GetUserClaims extends AbstractCandourHttpAuthenticationAction {
     @Nonnull
     private final Logger log = LoggerFactory.getLogger(GetUserClaims.class);
 
-    /** Candour API location. */
-    private URI candouridURI;
-
-    /** Candour API client public key. */
-    private String clientPublicKey;
-
-    /** Candour API client hmac key. */
-    private String clientHmacKey;
-
-    /**
-     * Set Candour API location.
-     * 
-     * @param uri Candour API location
-     * @throws URISyntaxException
-     */
-    public void setCandouridURI(String uri) throws URISyntaxException {
-        candouridURI = new URI(uri);
-    }
-
-    /**
-     * Set Candour API client public key.
-     * 
-     * @param publicKey Candour API client public key
-     */
-    public void setClientPublicKey(String publicKey) {
-        clientPublicKey = publicKey;
-    }
-
-    /**
-     * Set Candour API client hmac key.
-     * 
-     * @param hmacKey Candour API client hmac key
-     */
-    public void setClientHmacKey(String hmacKey) {
-        clientHmacKey = hmacKey;
-    }
-
     /** {@inheritDoc} */
     @Override
     protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext,
             @Nonnull final AuthenticationContext authenticationContext) {
-        CandourResultRequest message = new CandourResultRequest(candouridURI, clientPublicKey, clientHmacKey);
+        CandourResultRequest message = new CandourResultRequest(getCandouridURI(), getClientPublicKey(),
+                getClientHmacKey());
         message.setPayload(candourContext.getSessionId());
         CandourResponse response = null;
         try {
@@ -85,14 +55,13 @@ public class GetUserClaims extends AbstractCandourHttpAuthenticationAction {
         } catch (IOException | InvalidKeyException | NoSuchAlgorithmException | IllegalStateException
                 | URISyntaxException e) {
             log.error("{} Exception occurred", getLogPrefix(), e);
-            ActionSupport.buildEvent(profileRequestContext, EventIds.IO_ERROR);
+            ActionSupport.buildEvent(profileRequestContext, CandourEventIds.CANDOUR_API_COMM_FAILURE);
             return;
         }
         if (!response.indicateSuccess()) {
-            log.error("{} Candour response status code {} and payload {}", getLogPrefix(), response.getCode(),
-                    response.getPayload());
-            // TODO: use candour specific error event
-            ActionSupport.buildEvent(profileRequestContext, EventIds.IO_ERROR);
+            log.error("{} Candour invitation response indicates error. Status code {}, payload {}", getLogPrefix(),
+                    response.getCode(), response.getPayload());
+            ActionSupport.buildEvent(profileRequestContext, CandourEventIds.CANDOUR_API_RESP_FAILURE);
             return;
         }
         try {
@@ -100,13 +69,10 @@ public class GetUserClaims extends AbstractCandourHttpAuthenticationAction {
                     new ObjectMapper().readValue(response.getPayload(), new TypeReference<Map<String, Object>>() {
                     }));
         } catch (JsonProcessingException e) {
-            log.error("{} Candour response claims parsing failed.", getLogPrefix(), e);
-            // TODO: use candour specific error event
-            ActionSupport.buildEvent(profileRequestContext, EventIds.IO_ERROR);
+            log.error("{} Candour response parsing failed.", getLogPrefix(), e);
+            ActionSupport.buildEvent(profileRequestContext, CandourEventIds.CANDOUR_API_RESP_MALFORMED);
             return;
         }
-        //TODO: Next action should validate result claims like invitation and status
-        log.debug("{} Successfully received candour response payload {}", getLogPrefix(), response.getPayload());
 
     }
 
